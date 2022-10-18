0965a52af29e55b0f78e86074894f616c15036bb
Dejw7n <dejw7n@gmail.com>
2022-10-18 16:18:14 +0200

    Create traffic module

---
+++ src/app/traffic/traffic.module.ts
@@ -0,0 +1,9 @@
+import { HomeModule } from "./pages/home/home.module";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+	declarations: [],
+	imports: [HomeModule, CommonModule],
+})
+export class TrafficModule {}
