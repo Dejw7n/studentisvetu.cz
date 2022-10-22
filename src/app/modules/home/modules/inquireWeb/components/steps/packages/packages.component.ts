d34957db6644e3b729277ed895cbc2ffd6a83270
Dejw7n <dejw7n@gmail.com>
2022-10-22 12:53:00 +0200

    Create inquireWeb packages,, details, summary steps

---
+++ src/app/modules/home/modules/inquireWeb/components/steps/packages/packages.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "home-inquireWeb-packages",
+	templateUrl: "./packages.component.html",
+	styleUrls: ["./packages.component.scss"],
+})
+export class PackagesComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
--- src/app/modules/home/modules/inquireWeb/inquireWeb.module.ts
+++ src/app/modules/home/modules/inquireWeb/inquireWeb.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 import { InquireWebComponent } from "./components/inquireWeb/inquireWeb.component";
 import { PackagesComponent } from "./components/steps/packages/packages.component";
 import { DetailsComponent } from './components/steps/details/details.component';
+import { SummaryComponent } from './components/steps/summary/summary.component';
 
 @NgModule({
-	declarations: [InquireWebComponent, PackagesComponent, DetailsComponent],
+	declarations: [InquireWebComponent, PackagesComponent, DetailsComponent, SummaryComponent],
 	imports: [CommonModule],
 	exports: [InquireWebComponent],
 })
