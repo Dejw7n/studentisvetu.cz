fc88815b93be412d6fcb221529916e3181317a92
Dejw7n <dejw7n@gmail.com>
2022-10-23 17:44:33 +0200

    Create notebook-svg component

---
+++ src/app/modules/home/components/notebook-svg/notebook-svg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notebook-svg',
+  templateUrl: './notebook-svg.component.html',
+  styleUrls: ['./notebook-svg.component.scss']
+})
+export class NotebookSvgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
