e8d6205c9e06f93984ae69747fcc684b4ee7dd94
Dejw7n <dejw7n@gmail.com>
2022-10-23 17:42:38 +0200

    Update home module declarations

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,9 +3,12 @@ import { HomeComponent } from "./pages/home/home.component";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { InquireWebModule } from "./modules/inquireWeb/inquireWeb.module";
+import { DesktopSvgComponent } from './components/desktop-svg/desktop-svg.component';
+import { MobileSvgComponent } from './components/mobile-svg/mobile-svg.component';
+import { BrowserSvgComponent } from './components/browser-svg/browser-svg.component';
 
 @NgModule({
-	declarations: [HomeComponent],
+	declarations: [HomeComponent, DesktopSvgComponent, MobileSvgComponent, BrowserSvgComponent],
 	imports: [HomeRoutingModule, InquireWebModule, CommonModule],
 	exports: [HomeComponent],
 })
